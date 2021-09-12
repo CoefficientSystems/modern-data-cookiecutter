02e0f3b66a29ca319492da50d436ef046aa9140e
John Sandall <2884159+john-sandall@users.noreply.github.com>
2021-09-12 23:18:42 +0100

    Add .env and .env.template; and post-hook to copy .env.template -> .env

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import os
+import shutil
 from pathlib import Path
 
 PROJECT_DIRECTORY = Path(".")
@@ -10,6 +11,9 @@ if __name__ == "__main__":
 
     # Rename pyproject-template.toml -> pyproject.toml
     os.rename(PROJECT_DIRECTORY / "pyproject-template.toml", PROJECT_DIRECTORY / "pyproject.toml")
+
+    # Copy .env.template -> .env
+    shutil.copy(PROJECT_DIRECTORY / '.env.template', PROJECT_DIRECTORY / '.env')
 
     if "{{ cookiecutter.use_towncrier }}" != "y":
         os.remove(PROJECT_DIRECTORY / "docs" / "using_towncrier.md")
