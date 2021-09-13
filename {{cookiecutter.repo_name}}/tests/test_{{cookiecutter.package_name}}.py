9f14b6656f2f7a8d9024e04c8889a9e432065b65
John Sandall <2884159+john-sandall@users.noreply.github.com>
2021-09-13 15:40:20 +0100

    s/39/py39/ on black & pyupgrade versions

--- {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.package_name}}.py
+++ {{cookiecutter.repo_name}}/tests/test_{{cookiecutter.package_name}}.py
@@ -4,4 +4,4 @@ from {{cookiecutter.package_name}} import __version__
 
 
 def test_version():
-    assert __version__ == '{{ cookiecutter.version }}'
+    assert __version__ == "{{ cookiecutter.version }}"
