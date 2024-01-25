b59b890dda2692b64b5d58c8f72a49b42dc99b22
Thomas Grainger <thomas.grainger@coefficientai.onmicrosoft.com>
2024-01-25 14:45:39 +0000

    apply ruff

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,7 +6,6 @@ Read more: https://cookiecutter.readthedocs.io/en/1.7.3/advanced/hooks.html
 from __future__ import annotations
 
 import logging
-import os
 import shutil
 from pathlib import Path
 
@@ -16,14 +15,14 @@ if __name__ == "__main__":
     # Rename templated files
     (PROJECT_DIRECTORY / "pyproject.template.toml").rename(PROJECT_DIRECTORY / "pyproject.toml")
     (PROJECT_DIRECTORY / ".pre-commit-config.template.yaml").rename(
-        PROJECT_DIRECTORY / ".pre-commit-config.yaml"
+        PROJECT_DIRECTORY / ".pre-commit-config.yaml",
     )
 
     # Copy .env.template -> .env
     shutil.copy(PROJECT_DIRECTORY / ".env.template", PROJECT_DIRECTORY / ".env")
 
     if "{{ cookiecutter.use_towncrier }}" != "y":
-        os.remove(PROJECT_DIRECTORY / "docs" / "using_towncrier.md")
+        (PROJECT_DIRECTORY / "docs" / "using_towncrier.md").unlink()
 
     if "{{ cookiecutter.use_github_actions }}" != "y":
         try:
